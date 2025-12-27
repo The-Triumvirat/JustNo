574fc4be6d9cd41cb587ddd9d0434a7bd5527ecf
Magnus Hoffmann <Magnus-Hoffmann@web.de>
2025-12-27 21:23:28 +0100

    Build auth pages and coltrollers

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,11 @@ use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\Backoffice\BackofficeController;
 
+use App\Http\Controllers\BackofficeAuth\BackofficeLoginController;
+use App\Http\Controllers\BackofficeAuth\BackofficeLogoutController;
+
+use App\Http\Middleware\RedirectIfAuthenticatedCustom;
+
 Route::get('/', function () {
     return view('welcome');
 });
@@ -24,6 +29,12 @@ Route::middleware(['auth', 'role:admin'])
     ->prefix('/backoffice')
     ->group(function () {
         Route::get('/dashboard', [BackofficeController::class, 'backofficeDashboard'])->name('backoffice.dashboard');
+        Route::get('/logout', [BackofficeLogoutController::class, 'backofficeLogout'])->name('backoffice.logout');
     }); // End Group Backoffice Middleware
+
+// Route Accessable for All 
+Route::get('/backoffice/login', [BackofficeLoginController::class, 'backofficeLogin'])->name('backoffice.login')->middleware(RedirectIfAuthenticatedCustom::class);
+Route::get('/backoffice/forget', [BackofficeRestPasswordController::class, 'backofficeForgetPassword'])->name('backoffice.password.request')->middleware(RedirectIfAuthenticatedCustom::class);
+Route::get('/backoffice/reset-password/{token}', [BackofficeRestPasswordController::class, 'backofficeResetPassword'])->name('backoffice.password.reset')->middleware(RedirectIfAuthenticatedCustom::class);
 
 require __DIR__.'/auth.php';
