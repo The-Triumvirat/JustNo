6ec816c6acf713221d3c2fb28e9a64f5f9dd4386
Magnus Hoffmann <Magnus-Hoffmann@web.de>
2025-12-30 12:31:35 +0100

    Header clean up

--- resources/views/backoffice/_body/header.blade.php
+++ resources/views/backoffice/_body/header.blade.php
@@ -79,12 +79,6 @@
           </li>
           <li><a class="dropdown-item d-flex align-items-center" href="{{ route('backoffice.change.password') }}"><i class="bx bx-lock fs-5"></i><span>Change Password </span></a>
           </li>
-          <li><a class="dropdown-item d-flex align-items-center" href="javascript:;"><i class="bx bx-home-circle fs-5"></i><span>Dashboard</span></a>
-          </li>
-          <li><a class="dropdown-item d-flex align-items-center" href="javascript:;"><i class="bx bx-dollar-circle fs-5"></i><span>Earnings</span></a>
-          </li>
-          <li><a class="dropdown-item d-flex align-items-center" href="javascript:;"><i class="bx bx-download fs-5"></i><span>Downloads</span></a>
-          </li>
           <li>
             <div class="dropdown-divider mb-0"></div>
           </li>
