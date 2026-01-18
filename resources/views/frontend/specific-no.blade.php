80caad150f71e05317788d0048a2c2003eea2970
Magnus Hoffmann <Magnus-Hoffmann@web.de>
2026-01-18 21:15:33 +0100

    Move css file into ressources folder

--- resources/views/frontend/specific-no.blade.php
+++ resources/views/frontend/specific-no.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <title>JustNo</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="{{ asset('frontend/style.css') }}" rel="stylesheet">
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 
 <body>
