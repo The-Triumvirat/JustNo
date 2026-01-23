7f9c05ef7fbb957ee3ed67e4651351b69ac5fe2d
markhoff28 <Magnus-Hoffmann@web.de>
2026-01-23 16:27:15 +0100

    TT-lion as favicon

--- resources/views/backoffice/auth/backoffice_auth.blade.php
+++ resources/views/backoffice/auth/backoffice_auth.blade.php
@@ -5,7 +5,7 @@
     <title>@yield('titleAuthPage', 'Backoffice')</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <link rel="icon" href="{{ asset('backoffice/assets/images/favicon-32x32.png') }}" type="image/png" />
+    <link rel="icon" href="{{ asset('brand/tt-lion.png') }}" type="image/png" />
     <link href="{{ asset('backoffice/custom/css/toastr.css') }}" rel="stylesheet">
 
     @vite(['resources/css/app.css', 'resources/js/app.js'])
